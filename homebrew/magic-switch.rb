00e679d929b8c294428df8959de037493ac1a32b
cutsome <nissikpu@gmail.com>
2026-02-25 10:53:17 +0900

    Update sha256 for v1.1.1

--- homebrew/magic-switch.rb
+++ homebrew/magic-switch.rb
@@ -1,6 +1,6 @@
 cask "magic-switch" do
   version "1.1.1"
-  sha256 :no_check
+  sha256 "7c7fe339404fed684af22d9034d1bd11821568ee874e5946669f59074e0088eb"
 
   url "https://github.com/byteflare-co/magic-switch/releases/download/v#{version}/MagicSwitch-#{version}.zip"
   name "Magic Switch"
