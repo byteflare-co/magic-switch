6acd4d3cb70067f649ab0867e01157b2bbf90520
cutsome <nissikpu@gmail.com>
2026-02-25 18:19:52 +0900

    Bump version to 1.1.2

--- homebrew/magic-switch.rb
+++ homebrew/magic-switch.rb
@@ -1,5 +1,5 @@
 cask "magic-switch" do
-  version "1.1.1"
+  version "1.1.2"
   sha256 "7c7fe339404fed684af22d9034d1bd11821568ee874e5946669f59074e0088eb"
 
   url "https://github.com/byteflare-co/magic-switch/releases/download/v#{version}/MagicSwitch-#{version}.zip"
